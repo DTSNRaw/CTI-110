0de6fa8bc048c49e04035208c7b207c63d44d1c0
DTSNRaw <dotsond3455@student.faytechcc.edu>
2024-09-19 10:34:44 -0400

    LAB1

---
+++ P1Lab1_Damian_dotson.py
@@ -0,0 +1,13 @@
+# Your Name Damian Dotson
+ # Date 9/17/2024
+ # Assignment Name P1Lab1
+ # Get input from user, and display output.
+
+# get firstname from user
+first_name = input ("Enter your first name: ")
+
+# get last name from user
+last_name = input ("Enter your last name: ")
+
+# Display info to user
+print ("Hello,", first_name, last_name + "!", "welcome to CTI-110!")
