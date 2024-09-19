8f8a16fe5f7234702d8ab48804721096ec2e4359
DTSNRaw <dotsond3455@student.faytechcc.edu>
2024-09-19 10:29:18 -0400

    These are my first python code P1LAB1

---
+++ P1HW1.py
@@ -0,0 +1,11 @@
+ # Damian Dotson
+ # Date 9/17/2024
+ # Assignment Name p1HW1
+ # Input and output with mathematical expressions.
+
+ print ("-----Calculating Exponents-----")
+ print ()
+ print ()
+
+ #Get base value (as a integer) from the user
+ base_value = int(input ("Enter an integer as a base value: "))
